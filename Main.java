9354d8cd807df3b0bd7f51086f174b54ea6c532f
santhiyaramar <santhiya165mca@gmail.com>
2023-05-27 09:13:53 -0400

    commited by sandy

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+int salary;
+
+void teach(int salary)
+{
+salary = 10;
+System.out.println("teaching"+this.salary);
+System.out.println(salary);
+}
+public static void main (String[] args)
+{
+Main teacher = new Main();
+teacher.salary = 20000;
+teacher.teach(teacher.salary);
+}
+}
